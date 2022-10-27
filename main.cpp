e1035b020c753681ce0ea1d1dd35462da5bcf679
paarth-sharma <paarthsharma2003@gmail.com>
2022-10-27 11:17:59 +0530

    added the method to decrypt the message and display it in a txt file

--- main.cpp
+++ main.cpp
@@ -135,6 +135,117 @@ int main(){
 		cout<<"128-bit AES encryption is done sucessfully\n";
 		cout<<"Data has been appended to file encryption.aes";
 		newfile.close(); //close the file object.
-   }
-  break;
+   		}
+  	    break;
+	    }
+		case 2:{
+			cout<<"Reading encrypted data from encryption.txt .........\n";
+			Sleep(1000);
+
+			string tp;
+			cout<<"Reading KEY from key.txt ......\n";
+			Sleep(1000);
+
+			cout<<"Now Decrypting ....\n";
+			Sleep(1000);
+
+			cout<<"writing decrypted data in outputtext.txt ..\n";
+			Sleep(1000);
+			cout<<endl;
+
+			cout<<"Following is our decrypted text:-"<<endl;
+			//clearing outputtext file
+
+			ifstream File;
+			string filepath = "outputtext.txt";
+			File.open(filepath.c_str(), std::ifstream::out | std::ifstream::trunc );
+			if (!File.is_open() || File.fail()){
+				File.close();
+				printf("\nError : failed to erase file content !");
+			}
+			File.close();
+
+			ifstream MyReadFile;
+			MyReadFile.open("encryption.aes", ios::in | ios::binary);
+			if(MyReadFile.is_open()){
+				while(getline (MyReadFile, myText)){
+					cout.flush();
+					char * x;
+					x=&myText[0];
+					int messlength=strlen(x);
+					char * msg = new char[myText.size()+1];
+
+					strcpy(msg, myText.c_str());
+
+					int n = strlen((const char*)msg);
+					unsigned char * decryptedtext = new unsigned char[n];
+
+					//decrypting our encrypted data
+					for (int i = 0; i < n; i++)
+						decryptedtext[i] = (unsigned char)msg[i];
+					
+					//reading key from key.txt file
+					string k;
+					ifstream infile;
+					infile.open("key.txt");
+					if (infile.is_open()){
+						getline(infile, k); // The first line of file should be the key
+						infile.close();
+					}
+
+					else cout << "Unable to open file";
+
+					istringstream tempkey(k);
+					unsigned char key[16];
+					unsigned int x1;
+					for(int i=0;i<16;i++){
+						tempkey>>hex>>x1;
+						key[i] = x1;
+					}
+
+					//extending key
+					unsigned char extendedkeys[176];
+					Key_extenxion(key,extendedkeys);
+
+					//decrypting our data  
+					for (int i = 0; i < messlength; i += 16){
+						unsigned char * temp=new unsigned char[16];
+
+						for(int j=0;j<16;j++)
+							temp[j]=decryptedtext[i+j];
+
+						decryption(temp , extendedkeys);
+
+						for(int j=0;j<16;j++)
+							decryptedtext[i+j]=temp[j];
+					}
+					//printing our plain text
+					for(int i=0;i<messlength;i++){
+						cout<<decryptedtext[i];
+						if(decryptedtext[i]==0 && decryptedtext[i-1]==0)
+							break;
+					}
+					//storing plain text in outputtext.txt file
+					cout<<endl;
+					ofstream fout;  // Create Object of Ofstream
+					ifstream fin;
+					fin.open("outputtext.txt");
+					fout.open ("outputtext.txt",ios::app); // Append mode
+					if(fin.is_open())
+						fout<<decryptedtext<<"\n"; // Writing data to file
+
+					fin.close();
+					fout.close(); // Closing the file
+					Sleep(500);
+				}
+			}
+			else
+				cout<<"Can not open input file\n ";
+			
+			cout<<"\n Data has been appended to file outputtext.txt";
+			MyReadFile.close();
+			break;
+		}
+	}
 }
+	
